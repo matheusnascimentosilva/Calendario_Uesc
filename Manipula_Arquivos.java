7168598baf2a93c7b4650905feda2799e0711968
MATHEUS NASCIMENTO SILVA <34199239+matheusnascimentosilva@users.noreply.github.com>
2020-01-14 22:42:10 -0300

    Classe responsável por manipulação de arquivo
    Em desenvolvimento

---
+++ Manipula_Arquivos.java
@@ -0,0 +1,38 @@
+package BancoDeDados;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Manipula_Arquivos {
+	
+	
+	public static void Leitura(String path) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+ 
+            } else
+                break;
+            linha = buffRead.readLine();
+        }
+        buffRead.close();
+    }
+	
+	public static void Escrita(String path) throws IOException {
+        BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+        String linha = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Escreva algo: ");
+        linha = in.nextLine();
+        buffWrite.append(linha + "\n");
+        buffWrite.close();
+    }
+	
+
+}
