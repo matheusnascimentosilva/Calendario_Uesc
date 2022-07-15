b162b182a28ac5f5232e3091c1920607843d5490
MATHEUS NASCIMENTO SILVA <34199239+matheusnascimentosilva@users.noreply.github.com>
2022-07-15 14:15:30 -0300

    Update Testa_Banco.java

--- Testa_Banco.java
+++ Testa_Banco.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Testa_Banco {
 
 	public static void main(String[] args) throws IOException {
-		String path = "C:\\Users\\Matheus\\eclipse-workspace\\Projeto LPIII\\src\\BancoDeDados\\BancoEventos.Cal";
+		String path = "src\\BancoDeDados\\BancoEventos.Cal";
 		@SuppressWarnings("unused")
 		Manipula_Arquivos novo = new Manipula_Arquivos(); 
         //Manipula_Arquivos.Escrita(path);
